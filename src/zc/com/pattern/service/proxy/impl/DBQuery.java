1af37e4c071ce784d10f99ebbcfcc51d4a5ad5b3
a616079263 <zhou_chao_zc@163.com>
2017-03-07 12:02:09 +0800

    代理模式

--- src/zc/com/pattern/service/proxy/impl/DBQuery.java
+++ src/zc/com/pattern/service/proxy/impl/DBQuery.java
@@ -4,8 +4,17 @@ import zc.com.pattern.service.proxy.IDBQuery;
 
 /**
  * Created by Administrator on 2017/3/7.
+ * 真实主题
  */
 public class DBQuery implements IDBQuery {
+
+    public DBQuery() {
+        try {
+            Thread.sleep(1000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public String request() {
