07dc879b3cfb0226903b3ede797e358da318e029
a616079263 <zhou_chao_zc@163.com>
2017-03-08 15:24:17 +0800

    JDK代理类

--- src/zc/com/pattern/service/proxy/impl/JdkDbQeueryHandler.java
+++ src/zc/com/pattern/service/proxy/impl/JdkDbQeueryHandler.java
@@ -4,9 +4,11 @@ import zc.com.pattern.service.proxy.IDBQuery;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 /**
  * Created by Administrator on 2017/3/7.
+ * JDK自带的代理类
  */
 public class JdkDbQeueryHandler implements InvocationHandler {
     IDBQuery real = null;  //主题接口
@@ -16,5 +18,13 @@ public class JdkDbQeueryHandler implements InvocationHandler {
             real = new DBQuery();
         }
         return real.request();
+    }
+
+    public static IDBQuery createJdkproxy(){
+        IDBQuery jdkProxy =
+                (IDBQuery) Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(),
+                new Class[]{IDBQuery.class},
+                new JdkDbQeueryHandler());
+        return jdkProxy;
     }
 }
